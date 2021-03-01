64752b5053bdad2e237428b644c26bda394c4246
Alvaro-Neto <77589191+Alvaro-Neto@users.noreply.github.com>
2021-03-01 09:46:16 -0300

    Add files via upload
    Exercicio 3 GFT Start Uni

---
+++ Exercicio3/Intervalo.java
@@ -0,0 +1,32 @@
+package Exercicio3;
+
+import java.util.Scanner;
+
+public class Intervalo {
+	
+	
+	
+	
+	public static void main (String args[]) {
+		
+		System.out.println("Digite o intervalo inicial ");
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		
+		System.out.println("Digite o intervalo final ");
+		int num2 = scan.nextInt();
+		
+		int interv = num2 - num1;
+		
+		if ( interv > 15) {
+            System.out.println("\nIntervalo inicial: " + num1 + "| Intervalo final: " + num2 + "(intervalo = " + interv + ", inválido)");
+        }else {
+        	System.out.println("\nIntervalo inicial: " + num1 + "| Intervalo final: " + num2 + "(intervalo = " + interv + ", válido)");
+        }
+
+		
+		
+		
+	}
+
+}
