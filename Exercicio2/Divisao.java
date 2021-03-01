4eb9f37add972d9f009dfb74a0903aa8018ef9a9
Alvaro-Neto <77589191+Alvaro-Neto@users.noreply.github.com>
2021-03-01 09:45:26 -0300

    Add files via upload
    Exercicio 2 GFT Start Uni

---
+++ Exercicio2/Divisao.java
@@ -0,0 +1,18 @@
+package Exercicio2;
+
+public class Divisao {
+	
+	public static void main (String args[]) {
+		
+		for (int i = 10; i <= 215; i++) {
+			if(i%3 == 0) {
+				System.out.println(i);
+				
+			}
+			if (i%7 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
